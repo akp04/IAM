bf8065cfb86268bf48c2decd9f7843e696df9c0e
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-12 21:03:50 +0530

    Check application connectivity

--- src/sailpointproject/CheckApplicationConnectivity.java
+++ src/sailpointproject/CheckApplicationConnectivity.java
@@ -1,5 +1,35 @@
 package sailpointproject;
 
-public class CheckApplicationConnectivity {
+import sailpoint.api.SailPointContext;
+import sailpoint.object.Application;
+import sailpoint.tools.GeneralException;
 
+import sailpoint.connector.Connector;
+import sailpoint.connector.ConnectorFactory;
+
+
+
+/*
+ * Author 	 : Aakash Pandita
+ * output 	 : we need to check connectivity for given Application
+ * Arguments 	 : applicationName
+ */
+public class CheckApplicationConnectivity 
+{
+	Connector connector = null;
+	
+	public void execute(SailPointContext context, String appName) throws GeneralException
+	{
+		Application app = context.getObjectByName(Application.class, appName);
+		try
+		{
+			connector = ConnectorFactory.getConnector(app, null);
+			connector.testConfiguration();
+		}
+		catch(Exception e)
+		{
+			log.error("Excetion during Test connectivity of app: " + appName);
+			log.error(e.getMessage());
+		}		
+	}
 }
