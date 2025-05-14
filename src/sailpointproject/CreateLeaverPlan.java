13dd812def8ec8fd8e46ced992088367d8e4abcf
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-14 23:26:35 +0530

    Create leaver plan

---
+++ src/sailpointproject/CreateLeaverPlan.java
@@ -0,0 +1,16 @@
+package sailpointproject;
+
+
+/*
+ * Author 	: Aakash Pandita
+ * Arguments 	: identity, context
+ * Output	: we need to create a plan for Leaver UseCase - Life Cycle Event (LCE)
+ */
+public class CreateLeaverPlan 
+{
+	
+	// step 1 : raise account request to disable/delete accounts - loop through all apps
+	// step 2 : remove all business roles, birthright roles will be revoked automatically
+	// step 3 : create account request for modify and add attribute requests in it to remove access
+	
+}
