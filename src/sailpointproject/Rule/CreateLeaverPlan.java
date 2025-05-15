9c3bc7d04dbd3fe1421e61cfeff6d0ba4ea729a6
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-15 23:44:39 +0530

    Create leaver plan

--- src/sailpointproject/Rule/CreateLeaverPlan.java
+++ src/sailpointproject/Rule/CreateLeaverPlan.java
@@ -1,5 +1,6 @@
 package sailpointproject.Rule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sailpoint.api.SailPointContext;
@@ -7,6 +8,8 @@ import sailpoint.object.Application;
 import sailpoint.object.Identity;
 import sailpoint.object.Link;
 import sailpoint.object.ProvisioningPlan;
+import sailpoint.object.ProvisioningPlan.AccountRequest;
+import sailpoint.object.ProvisioningPlan.AttributeRequest;
 import sailpoint.tools.GeneralException;
 
 /*
@@ -17,9 +20,9 @@ import sailpoint.tools.GeneralException;
 public class CreateLeaverPlan 
 {
 	
-	// step 1 : raise account request to disable/delete accounts - loop through all apps
-	// step 2 : remove all business roles, birthright roles will be revoked automatically
-	// step 3 : create account request for modify and add attribute requests in it to remove access
+	// step 1 : disable account and remove access
+	// step 2 : remove workgroups
+	// step 3 : remove roles
 	
 	final String AUTHORITATIVE_APP = "HR App";
 	final String AD_APP_TYPE = "Active Directory - Direct";
@@ -29,6 +32,7 @@ public class CreateLeaverPlan
 	{
 		plan.setIdentity(identity);
 		List<Link> links = identity.getLinks();
+		String ENTITLEMENT_ATTRIBUTE = "";
 		
 		for(Link link : links)
 		{
@@ -42,25 +46,56 @@ public class CreateLeaverPlan
 				continue;
 			}
 			
-			// if application type is AD
+			// step 1 - if application type is AD
 			if(app.getType().equalsIgnoreCase(AD_APP_TYPE))
 			{
+				ENTITLEMENT_ATTRIBUTE = "memberOf";
+				AccountRequest accReq = new AccountRequest( AccountRequest.Operation.Disable, appName, null, nativeId );
+				plan.add(accReq);
 				
+				accReq = new AccountRequest();
+				accReq.setOperation(AccountRequest.Operation.Modify);
+				accReq.setNativeIdentity(nativeId);
+				accReq.setApplication(appName);
+				
+				// fetch all the entitlements for the identity of this application
+				List<AttributeRequest> attrReqs = getAttributeList(identity, appName, ENTITLEMENT_ATTRIBUTE);
+				accReq.setAttributeRequests(attrReqs);
+				plan.add(accReq);
+				
+			}			
+			// if application type is something else then need to generate plan according to the application
+			
+			
+			// step 2 - remove workgroups
+			List<Identity> wgs = identity.getWorkgroups();
+			if( null != wgs)
+			{
+				for(Identity wg : wgs)
+				{
+					
+				}
 			}
+			
+			
+			// step 3 - remove roles
 			
 		}
 	}
-	
-	
-	// disable account and remove access
-	
-	
-	
-	// remove workgroups
-	
-	
-	
-	// remove roles
-	
-	
+
+	private List<AttributeRequest> getAttributeList(Identity identity, String appName, String ENTITLEMENT_ATTRIBUTE) 
+	{
+		// TODO Auto-generated method stub
+		Link link = identity.getLink(appName);
+		List<AttributeRequest> attrReqs = new ArrayList();
+		List<String> groups = (List<String>) link.getAttribute(ENTITLEMENT_ATTRIBUTE);
+		
+		for(String grp : groups )
+		{
+			attrReqs.add(new AttributeRequest( ENTITLEMENT_ATTRIBUTE, ProvisioningPlan.Operation.Remove, grp  ));
+		}
+
+		return attrReqs;
+	}
+
 }
