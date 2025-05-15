78d892a53a3f8ce4b8a3abbe9a0ffa477548e718
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-15 23:02:45 +0530

    Create leaver plan

--- src/sailpointproject/Rule/CreateLeaverPlan.java
+++ src/sailpointproject/Rule/CreateLeaverPlan.java
@@ -1,5 +1,13 @@
 package sailpointproject.Rule;
 
+import java.util.List;
+
+import sailpoint.api.SailPointContext;
+import sailpoint.object.Application;
+import sailpoint.object.Identity;
+import sailpoint.object.Link;
+import sailpoint.object.ProvisioningPlan;
+import sailpoint.tools.GeneralException;
 
 /*
  * Author 	: Aakash Pandita
@@ -12,5 +20,47 @@ public class CreateLeaverPlan
 	// step 1 : raise account request to disable/delete accounts - loop through all apps
 	// step 2 : remove all business roles, birthright roles will be revoked automatically
 	// step 3 : create account request for modify and add attribute requests in it to remove access
+	
+	final String AUTHORITATIVE_APP = "HR App";
+	final String AD_APP_TYPE = "Active Directory - Direct";
+	ProvisioningPlan plan = new ProvisioningPlan();
+	
+	public void execute(SailPointContext context, Identity identity) throws GeneralException
+	{
+		plan.setIdentity(identity);
+		List<Link> links = identity.getLinks();
+		
+		for(Link link : links)
+		{
+			String nativeId = link.getNativeIdentity();
+			String appName = link.getApplicationName();
+			Application app = context.getObjectByName(Application.class, appName);
+			
+			// check if its Authoritative app
+			if(appName.equalsIgnoreCase(AUTHORITATIVE_APP)) 
+			{
+				continue;
+			}
+			
+			// if application type is AD
+			if(app.getType().equalsIgnoreCase(AD_APP_TYPE))
+			{
+				
+			}
+			
+		}
+	}
+	
+	
+	// disable account and remove access
+	
+	
+	
+	// remove workgroups
+	
+	
+	
+	// remove roles
+	
 	
 }
