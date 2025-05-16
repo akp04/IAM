f8ad828ccdf92e9f5c5e7988f9c73ed00a0f7ae4
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-16 23:06:17 +0530

    AccountGroup Refresh Rule

--- src/sailpointproject/Rule/MakeEntitlementRequestable.java
+++ src/sailpointproject/Rule/MakeEntitlementRequestable.java
@@ -1,5 +1,20 @@
 package sailpointproject.Rule;
 
-public class MakeEntitlementRequestable {
+import sailpoint.api.SailPointContext;
+import sailpoint.object.ManagedAttribute;
 
+
+/*
+ * Author 	: Aakash Pandita
+ * Arguments 	: identity, context
+ * Output	: we need to create a make entitlements requestable and set owners for them. we can also set level of approval as per environment
+ */
+public class MakeEntitlementRequestable 
+{
+	
+	
+	public void execute(SailPointContext context, String groupApplication, ManagedAttribute accountGroup)
+	{
+		
+	}
 }
