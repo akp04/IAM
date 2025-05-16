7f9fe49e62263955236f50ea50405b7ec8abe966
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-16 23:51:53 +0530

    AccountGroup Refresh Rule

--- src/sailpointproject/Rule/MakeEntitlementRequestable.java
+++ src/sailpointproject/Rule/MakeEntitlementRequestable.java
@@ -1,20 +1,36 @@
 package sailpointproject.Rule;
 
 import sailpoint.api.SailPointContext;
+import sailpoint.object.Identity;
 import sailpoint.object.ManagedAttribute;
+import sailpoint.tools.GeneralException;
 
 
 /*
  * Author 	: Aakash Pandita
  * Arguments 	: identity, context
- * Output	: we need to create a make entitlements requestable and set owners for them. we can also set level of approval as per environment
+ * Output	: we need to make entitlements requestable and set owners for them. we can also set level of approval as per environment
  */
 public class MakeEntitlementRequestable 
 {
-	
-	
-	public void execute(SailPointContext context, String groupApplication, ManagedAttribute accountGroup)
+	public ManagedAttribute execute(SailPointContext context, String groupApplication, ManagedAttribute accountGroup) throws GeneralException
 	{
+		String group_owner = "spadmin"; // this can be workgroup as well
+		Identity owner  = context.getObjectByName(Identity.class, group_owner);
+		String appName = "SaaS App";
 		
+		if(groupApplication.equalsIgnoreCase(appName))
+		{
+			if(null != accountGroup && !accountGroup.isRequestable())
+			{
+				accountGroup.setRequestable(true);
+				accountGroup.setOwner(owner);
+				accountGroup.setDisplayName(accountGroup.getDisplayName());
+				// accountGroup.setAttribute("Level_Of_Approval", "Two Level");
+				// accountGroup.setAttribute("Level 1", "");
+				// accountGroup.setAttribute("Level 2", "");
+			}
+		}
+		return accountGroup;
 	}
 }
