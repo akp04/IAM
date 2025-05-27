7686d3dbb71281480680f1f8c485c834429bccea
AAKASH PANDITA <aakash2711522@gmail.com>
2025-05-28 00:01:55 +0530

    Sailpoint Util class

--- src/sailpointproject/Util/SailpointUtil.java
+++ src/sailpointproject/Util/SailpointUtil.java
@@ -1,5 +1,17 @@
 package sailpointproject.Util;
 
-public class SailpointUtil {
+import java.util.Map;
+
+import sailpoint.api.SailPointContext;
+
+public class SailpointUtil 
+{
+	
+	
+	public void sendEmailNotifications(SailPointContext context, String to, String cc, Map args)
+	{
+		
+	}
+	
 
 }
